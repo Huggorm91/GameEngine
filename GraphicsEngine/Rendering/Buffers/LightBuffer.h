9179b458762537b80493ab0bb9bf357e853c652d
Huggorm91 <99368710+Huggorm91@users.noreply.github.com>
2023-08-26 01:40:01 +0200

    Added constant for Max amount of lights

--- GraphicsEngine/Rendering/Buffers/LightBuffer.h
+++ GraphicsEngine/Rendering/Buffers/LightBuffer.h
@@ -2,6 +2,8 @@
 #include <Matrix4x4.hpp>
 #include "Rendering/Buffers/ConstantBuffer.h"
 
+const unsigned MAX_LIGHTS = 8;
+
 struct PointlightData
 {
 	CommonUtilities::Vector3f myPosition;
@@ -31,8 +33,8 @@ struct LightBufferData
 	CommonUtilities::Vector3f myDirectionallightColor;
 	float myAmbientlightIntensity;
 
-	PointlightData myPointlights[8];
-	SpotlightData mySpotlights[8];
+	PointlightData myPointlights[MAX_LIGHTS];
+	SpotlightData mySpotlights[MAX_LIGHTS];
 
 #ifdef _DEBUG
 	int myLightMode;
