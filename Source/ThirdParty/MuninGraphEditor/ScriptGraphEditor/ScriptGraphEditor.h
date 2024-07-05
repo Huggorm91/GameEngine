a5158967d32b6b2ae20a88b583f3e4e2a06e2abf
Huggorm91 <olaus@klaveness.dev>
2024-07-05 22:31:33 +0200

    Test push

--- Source/ThirdParty/MuninGraphEditor/ScriptGraphEditor/ScriptGraphEditor.h
+++ Source/ThirdParty/MuninGraphEditor/ScriptGraphEditor/ScriptGraphEditor.h
@@ -97,7 +97,6 @@ protected:
 	void HandleBackgroundContextMenuItemClicked(const GraphEditorContextMenuItem& aItem) override;
 
 	void ReportError(const ScriptGraph* aGraph, size_t aNodeId, std::string_view aErrorMessage);
-	void ClearError();
 
 private:
 
